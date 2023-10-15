cc6aed31ed5ce0de78fbc238b37d91365cf457dd
adilakshmi367 <147702154+adilakshmi367@users.noreply.github.com>
2023-10-15 15:37:14 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello Aadi" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello Aadi!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello Aadi\"");
+            fail("\"message\" is not \"Hello Aadi!\"");
         }
     }
 
